ca72063b73b66d21ec020d9808c165275c25ada9
sierraskyewallace <sskwallace@gmail.com>
2021-08-09 15:05:27 -0700

    user serializer finsiehd

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
 
     def index 
         users = User.all
-        render json: UserSerializer.new(users)
+        render json: UserSerializer.new(users, options)
     end
 
     def create 
