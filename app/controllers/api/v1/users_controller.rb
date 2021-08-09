122d3af5b92ffeeb4e46c21c2eec7ee0ab345d50
sierraskyewallace <sskwallace@gmail.com>
2021-08-09 14:51:27 -0700

    added attributes to serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
 
     def index 
         users = User.all
-        render json: users
+        render json: UserSerializer.new(users)
     end
 
     def create 
