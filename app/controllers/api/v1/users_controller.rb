3b9f6e5251ebd1a954b7ff4a08cd93677a6a66c9
sierraskyewallace <sskwallace@gmail.com>
2021-08-26 17:44:47 -0700

    Adding jwt to app controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+ skip_before_action :authorized, only: [:create]
 
     def index 
         users = User.all
@@ -9,14 +10,15 @@ class Api::V1::UsersController < ApplicationController
 
     end
 
-    def create 
-        user = User.new(user_params)
-        if user.save
-            render json: { user: UserSerializer.new(user) }, status: :created
-        else
-            render json: { error: "Username is already taken" }, status: :not_acceptable
-        end
+    def create
+    user = User.create(user_params)
+    if user.valid?
+      @token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :unprocessable_entity
     end
+  end
 
     private 
 
