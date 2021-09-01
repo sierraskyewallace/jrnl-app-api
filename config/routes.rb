8ba5665a4952d167241325609dbc8f45cecf6670
sierraskyewallace <sskwallace@gmail.com>
2021-09-01 15:53:38 -0700

    postman test for sign up passing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
     namespace :api do
       namespace :v1 do
+        resources :users, only: [:index, :create]
         post '/login', to: 'auth#create'
         get '/profile', to: 'users#profile'
-        resources :users, only: [:index, :create]
         resources :journal_entries, only: [:index, :create]
     end
   end
