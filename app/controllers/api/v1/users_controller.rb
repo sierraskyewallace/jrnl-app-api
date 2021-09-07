a34f61cb7e3008998223bdf5150450f3f46d14f0
sierraskyewallace <sskwallace@gmail.com>
2021-09-06 18:02:36 -0700

    Added users controller and avtions with coimments

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController 
 
+    #Register
    def create 
    end
 
+   #Login
    def login
    end
 
+   private 
+
+   def user_params
+    params.require(:user).permit(:username, :password)
+   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::API
-  
+    
+    #check is user is current user
+    def current_user
+    end
+
+    #check if user is logged in
+    def logged_in?
+    end
+
+    #make sure currrent user is auth for page or action
+    def authorize 
+    end
+
 end
