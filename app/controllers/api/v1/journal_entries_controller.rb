21051b4ecce42a26354fd7b2f9a65383a0c19754
sierraskyewallace <sskwallace@gmail.com>
2021-09-08 13:21:16 -0700

    journal entries creating properly

--- app/controllers/api/v1/journal_entries_controller.rb
+++ app/controllers/api/v1/journal_entries_controller.rb
@@ -1,15 +1,22 @@
 class Api::V1::JournalEntriesController < ApplicationController
-    #skip_before_action :authorized, only: [:index]
+
 
     def index 
-        journal_entries = JournalEntry.all
+        current_user = User.find_by(params[:id])
+        journal_entries = current_user.journal_entries
         render json: JournalEntrySerializer.new(journal_entries)
     end
 
+    def new 
+        current_user = User.find_by(params[:id])
+        journal_entry = current_user.journal_entries.build
+    end
+
     def create 
-        journal_entry = JournalEntry.new(journal_entry_params)
+        current_user = User.find_by(params[:id])
+        journal_entry = current_user.journal_entries.build(journal_entry_params)
         if journal_entry.save
-            render json: journal_entry, status: :created
+            render json: JournalEntrySerializer.new(journal_entry), status: :created
         else
             render json: { errors: journal_entry.errors.full_messages }
         end
@@ -17,8 +24,11 @@ class Api::V1::JournalEntriesController < ApplicationController
 
     private 
 
+    def current_user
+        current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
     def journal_entry_params
-        params.require(:journal_entry).permit(:name, :content)
+        params.require(:journal_entry).permit(:name, :content, :user_id)
     end
 end
