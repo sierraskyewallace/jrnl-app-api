ee7a8bef099866f101a5513ef06b5385a7913791
sierraskyewallace <sskwallace@gmail.com>
2021-09-09 14:20:07 -0700

    user sign up and login working properly, user unique username only

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,36 +1,23 @@
 class Api::V1::UsersController < ApplicationController 
 
+        def index 
+            @users = User.all 
+        end
   
+def new
+    @user = User.new 
+end
 
-  def new
-    @user = User.new
-  end
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save!
       session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: :created
     else
-      render json: { message: "Unable to create a user. Please try again." }, status: :denied
+      render json: { message: "Unable to create a user. Please try again." }, status: :unprocessable_entity
     end
   end
-
-  def new
-    @user = User.new
-  end
-
-  def login
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      render json: UserSerializer.new(@user), status: :created
-    else
-      render json: { message: "Incorrect Username/Password." }, status: :denied
-    end
-  end
-
-
 
 
    private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-
-
     namespace :api do
       namespace :v1 do
         resources :users 
+        post '/login' => "sessions#create"
         resources :journal_entries, only: [:index, :create]
     end
   end
