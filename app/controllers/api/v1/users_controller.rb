52c0f0b26f6202919112c9f4ecbee947e1ce7dcd
sierraskyewallace <sskwallace@gmail.com>
2021-09-22 14:39:52 -0700

    Added devise and fixed methods/actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,36 +1,45 @@
 class Api::V1::UsersController < Devise::RegistrationsController
+
     
       def index 
             @users = User.all 
+            render json: UserSerializer.new(@users)
+
         end
-  
-def new
-    @user = User.new 
-end
+        def new 
+            @user = User.new
+            render json: UserSerializer.new(@user)
+        end
 
 
- def create 
-    @user = User.find_by(email: user_params[:email])
-    if @user && @user.valid_password?(user_params[:password])
-        sign_in @user
-      #session[:user_id] = @user.id
-      render json:  UserSerializer.new(@user), status: :created
-    elsif
-        @user = User.new(user_params)
-        if @user.save!
-            sign_in @user
-            #session[:user_id] = @user.id
-            render json: UserSerializer.new(@user), status: :created
+        def create 
+            ## check if user is already in the database
+            @user = User.find_by(email: params[:user][:email])
+            if @user && @user.valid_password?(params[:user][:password])
+                session[:user_id] = @user.id
+
+                render json: UserSerializer.new(@user), status: :accepted
+
+            else
+            @user = User.create(user_params)
+            if @user.save
+                session[:user_id] = @user.id
+                render json: UserSerializer.new(@user), status: :created
+            else
+                render json: {errors: @user.errors.full_messages}
+            end
         end
-    else
-        render json: { message: "Error" }, status: :unprocessable_entity
-    end
-end
+        end
+    
+
+
+    
+
 
     private
 
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
 devise_for :users, :controllers => { :registrations => "api/v1/users", :sessions => "api/v1/users" }
 devise_scope :user do
+  #post 'api/v1/users/sign_in' => 'api/v1/users#sign_in'
+  #get 'users/sign_in' => 'api/v1/users#index'
+  post 'users' => 'api/v1/users#create'
+ # post 'users' => 'api/v1/sessions#create'
     namespace :api do
       namespace :v1 do
-        post '/users', to: 'users#create'
+        resources :users
         resources :journal_entries, only: [:index, :create]
     end
   end
