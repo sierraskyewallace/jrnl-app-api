61a04d208efbb14c6440d1802632e9dc18878560
sierraskyewallace <sskwallace@gmail.com>
2021-09-23 16:51:08 -0700

    updating create users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,16 +4,23 @@ class Api::V1::UsersController < ApplicationController
             @users = User.all 
             render json: UserSerializer.new(@users)
         end
+
+        def new
+            @user = User.new
+        end
+
   
         def create
             ## check if user already exists and password matches and log in
-            @user = User.find_by(username: params[:username])
+            @user = User.find_by(username: user_params[:username])
             if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
                 render json: UserSerializer.new(@user)
             else
                 ## create new user
                 @user = User.new(user_params)
                 if @user.save!
+                    session[:user_id] = @user.id
                     render json: UserSerializer.new(@user)
                 else
                     render json: {error: @user.errors.full_messages}
@@ -23,8 +30,8 @@ class Api::V1::UsersController < ApplicationController
 
 
             def login 
-                @user = User.find_by(username: params[:username])
-                if @user && @user.authenticate(params[:password])
+                @user = User.find_by(username: user_params[:username])
+                if @user && @user.authenticate(user_params[:password])
                     render json: UserSerializer.new(@user)
                 else
                     render json: {error: "User not found"}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,21 @@
 class ApplicationController < ActionController::API
-    
 
 
-    def logged_in?
-        !!current_user
-    end
+
+    private
 
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
-    def authenticate
-        if !logged_in?
-            render json: { error: "Please log in" }
-        end
+    def logged_in?
+        !!current_user
+    end
 
+    def authenticate
+	    unless logged_in?
+	      render json: { message: 'Please log in' }, status: :unauthorized
+    	end
     end
     
 end
